d15b57e29c51461bccdac6c0e007f2394791aa83
ompethani007 <infinitie.om@gmail.com>
2026-08-17 12:33:32 +0530

    fix: add space below compact card discount

--- src/components/Cards/ProductCardCompact.tsx
+++ src/components/Cards/ProductCardCompact.tsx
@@ -42,7 +42,7 @@ const ProductCardCompact: FC<ProductCardLayoutProps> = (props) => {
         />
       </CardBody>
 
-      <CardFooter className="grow flex-col items-stretch gap-2 px-0 pb-0 pt-3">
+      <CardFooter className="grow flex-col items-stretch gap-2 px-0 pb-1 pt-3">
         <div className="flex items-center justify-between gap-2">
           <span className="truncate text-xs font-medium text-default-500">
             {product.category_name || product.brand_name}
@@ -65,7 +65,7 @@ const ProductCardCompact: FC<ProductCardLayoutProps> = (props) => {
         </Link>
 
         <div className="mt-auto flex items-start justify-between gap-3 border-t border-divider pt-3">
-          <div className="min-w-0 flex-1">
+          <div className="min-w-0 flex-1 pb-1">
             <ProductCardPrice pricing={pricing} stacked />
           </div>
           {showAddToCart ? (
